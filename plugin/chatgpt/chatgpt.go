3e337b1df61790a86c8d43ee754c7d4328c09fee
小宇宇~ <3061196825@qq.com>
2025-02-02 11:43:10 +0800

    添加插件

---
+++ plugin/chatgpt/chatgpt.go
@@ -0,0 +1,116 @@
+package chatgpt
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	// baseURL  = "https://api.openai.com/v1/"
+	//proxyURL           = "https://uu.ci/v1/chat/completions"
+	proxyURL           = "https://api.alioth.center/akasha-whisper/v1/chat/completions"
+	modelGPT3Dot5Turbo = "gpt3.5-turbo"
+	yunKey             = "7d06a110e9e20a684e02934549db1d3d"
+	yunURL             = "https://api.a20safe.com/api.php?api=35&key=%s&apikey=%s"
+)
+
+type yun struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data []struct {
+		Return    string `json:"return"`
+		Total     string `json:"total"`
+		Available string `json:"available"`
+		Used      string `json:"used"`
+	} `json:"data"`
+}
+
+// tts语言回复
+/*type ApifoxModel struct {
+	// 要生成音频的文本。最大长度为4096个字符。
+	Input string `json:"input"`
+	Model string `json:"model"`
+	// 生成音频时使用的语音。支持的语音有:alloy、echo、fable、onyx、nova 和 shimmer。
+	Voice string `json:"voice"`
+}
+*/
+// chatGPTResponseBody 响应体
+type chatGPTResponseBody struct {
+	ID      string       `json:"id"`
+	Object  string       `json:"object"`
+	Created int          `json:"created"`
+	Model   string       `json:"model"`
+	Choices []chatChoice `json:"choices"`
+	Usage   chatUsage    `json:"usage"`
+}
+
+// chatGPTRequestBody 请求体
+type chatGPTRequestBody struct {
+	Model    string        `json:"model,omitempty"` // gpt3.5-turbo || gpt-4
+	Messages []chatMessage `json:"messages,omitempty"`
+}
+
+// chatMessage 消息
+type chatMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type chatChoice struct {
+	Index        int `json:"index"`
+	Message      chatMessage
+	FinishReason string `json:"finish_reason"`
+}
+
+type chatUsage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+var client = &http.Client{
+	Transport: &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	},
+	Timeout: time.Minute * 5,
+}
+
+// completions gtp3.5文本模型回复
+func completions(messages []chatMessage, apiKey string, model string) (*chatGPTResponseBody, error) {
+	com := chatGPTRequestBody{
+		Messages: messages,
+		Model:    model,
+	}
+	body, err := json.Marshal(com)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", proxyURL, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", apiKey)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	/*req.Header.Set("Accept", "application/json; charset=utf-8")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiKey))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")*/
+	//client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
+		return nil, errors.New("response error: " + strconv.Itoa(res.StatusCode))
+	}
+	v := new(chatGPTResponseBody)
+	if err = json.NewDecoder(res.Body).Decode(&v); err != nil {
+		return nil, err
+	}
+	return v, nil
+}
---
+++ plugin/custom/custom.go
@@ -0,0 +1,128 @@
+// Package custom 自定义插件
+package custom
+
+import (
+	"encoding/json"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/FloatTech/floatbox/binary"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() {
+	engine := control.Register("custom", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault: false,
+		Brief:            "自订工具",
+		Help: " - /kill\n" +
+			" - /发送公告\n" +
+			" - @bot给主人留言<内容>\n" +
+			" - 模拟xx条消息",
+	})
+	engine.OnFullMatchGroup([]string{"pause", "restart", "/kill"}, zero.OnlyToMe, zero.SuperUserPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			os.Exit(0)
+		})
+	engine.OnRegex(`^模拟([0-9]+)条消息`, zero.SuperUserPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			var err error
+			e := ctx.Event
+			e.RawMessage = "虚拟事件"
+			e.Message = message.Message{message.Text("虚拟事件")}
+			e.NativeMessage = json.RawMessage("\"虚拟事件\"")
+			e.Time = time.Now().Unix()
+			vev, cl := binary.OpenWriterF(func(w *binary.Writer) {
+				err = json.NewEncoder(w).Encode(e)
+			})
+			defer cl()
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			i, err := strconv.ParseInt(ctx.State["regex_matched"].([]string)[1], 10, 64)
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR: ", err))
+				return
+			}
+			go func() {
+				for j := int64(0); j < i; j++ {
+					go ctx.Echo(vev)
+				}
+			}()
+		})
+	engine.OnFullMatch("发送公告", zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			next := zero.NewFutureEvent("message", 999, false, zero.OnlyGroup, ctx.CheckSession())
+			recv, stop := next.Repeat()
+			defer stop()
+			ctx.SendChain(message.Text("请输入公告内容"))
+			var step int
+			var origin string
+			for {
+				select {
+				case <-time.After(time.Second * 60):
+					ctx.SendChain(message.Text("时间太久啦！不发了！"))
+					return
+				case c := <-recv:
+					switch step {
+					case 0:
+						origin = c.Event.RawMessage
+						ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"来决定是否发送此公告"))
+						step++
+					case 1:
+						msg := c.Event.Message.ExtractPlainText()
+						if msg != "确定" && msg != "取消" {
+							ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"哟"))
+							continue
+						}
+						if msg == "确定" {
+							ctx.SendChain(message.Text("正在发送..."))
+							zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
+								for _, g := range ctx.GetGroupList().Array() {
+									gid := g.Get("group_id").Int()
+									ctx.SendGroupMessage(gid, origin)
+									time.Sleep(1 * time.Second)
+								}
+								for _, r := range ctx.GetFriendList().Array() {
+									uid := r.Get("user_id").Int()
+									ctx.SendPrivateMessage(uid, message.Text(origin))
+									time.Sleep(1 * time.Second)
+								}
+								return true
+							})
+							return
+						}
+						ctx.SendChain(message.Text("已经取消发送了哟~"))
+						return
+					}
+				}
+			}
+		})
+	engine.OnRegex(`给主人留言.*?(.*)`, zero.OnlyToMe).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			su := zero.BotConfig.SuperUsers[0]
+			now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
+			uid := ctx.Event.UserID
+			gid := ctx.Event.GroupID
+			username := ctx.CardOrNickName(uid)
+			botid := ctx.Event.SelfID
+			botname := zero.BotConfig.NickName[0]
+			rawmsg := ctx.State["regex_matched"].([]string)[1]
+			rawmsg = message.UnescapeCQCodeText(rawmsg)
+			msg := make(message.Message, 10)
+			msg = append(msg, message.CustomNode(botname, botid, "有人给你留言啦！\n在"+now))
+			if gid != 0 {
+				groupname := ctx.GetGroupInfo(gid, true).Name
+				msg = append(msg, message.CustomNode(botname, botid, "来自群聊:["+groupname+"]("+strconv.FormatInt(gid, 10)+")\n来自群成员:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是留言内容"))
+			} else {
+				msg = append(msg, message.CustomNode(botname, botid, "来自私聊:["+username+"]("+strconv.FormatInt(uid, 10)+")\n以下是留言内容:"))
+			}
+			msg = append(msg, message.CustomNode(username, uid, rawmsg))
+			ctx.SendPrivateForwardMessage(su, msg)
+		})
+}
